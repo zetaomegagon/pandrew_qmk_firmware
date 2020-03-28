1b584f7e789f0fddaeba6ec0325559773b609876
Tent Ator <tentator@hotmail.com>
2020-03-28 16:16:24 +0100

    adding all 10t8or_spacefn layouts by Tent Ator

---
+++ keyboards/xwhatsit/brand_new_model_f/f62/keymaps/10t8or_spacefn/rules.mk
@@ -0,0 +1,4 @@
+NKRO_ENABLE = yes                     # USB Nkey Rollover
+LINK_TIME_OPTIMIZATION_ENABLE = yes   # firmware will be quicker
+COMMAND_ENABLE = no                   # Commands for debug and configuration
+
